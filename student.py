5df86c8e940ddaee227edf0a7707d29758cb0e0c
robyn-you <robyn.yhj@gmail.com>
2026-09-21 19:17:51 +1000

    student.py

--- student.py
+++ student.py
@@ -6,3 +6,23 @@ class Student:
         self.studentId = studentId
         self.subjects = subjects if subjects is not None else []
 
+    def toRecord(self):
+        return {
+            "name":self.name, 
+            "email": self.email, 
+            "password":self.password, 
+            "studentId": self.studentId, 
+            "subjets": self.subjects,   
+         }
+
+
+#Jason, Dictionary -> student, object converting. Bring the value from the Recorded value. => allowing comparison 
+    @classmethod
+    def fromRecord(cls, record):
+        return cls (
+            name=record["name"],
+            email=record["email"], 
+            password=record["password"],
+            studentId=record["StudentId"],
+            subjects=record.get("subjects",[]),
+        )
